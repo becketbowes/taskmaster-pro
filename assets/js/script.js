9c3b51bf1b5b3cf0379a58a20620f15c8bfd5e65
Ismaelleon <Ismaelleon918@gmail.com>
2022-01-10 20:09:55 -0300

    feat: add task editing

--- assets/js/script.js
+++ assets/js/script.js
@@ -54,6 +54,7 @@ $("#task-form-modal").on("show.bs.modal", function() {
   $("#modalTaskDescription, #modalDueDate").val("");
 });
 
+
 // modal is fully visible
 $("#task-form-modal").on("shown.bs.modal", function() {
   // highlight textarea
@@ -81,6 +82,109 @@ $("#task-form-modal .btn-primary").click(function() {
     saveTasks();
   }
 });
+
+let currentTask = {
+  taskElement: undefined,
+  propertyElement: undefined,
+  index: undefined
+};
+
+// task date was clicked
+$("#list-toDo").on("click", "li > span", function(event) {
+  // if the date span does not already have an input element inside
+  if (event.currentTarget.firstElementChild === null) {
+    // get an array of tasks
+    let parentChildren = Array.from(event.currentTarget.childNodes);
+
+    // get index of the current task inside the childNodes of #list-toDo
+    currentTask.index = parentChildren.indexOf(event.currentTarget);
+
+    // create an input element inside the date span
+    event.currentTarget.innerHTML = `
+      <input type="text" value="${event.currentTarget.innerText}" 
+        style="border: none; background: none; font-size: inherit; 
+        color: inherit; padding: 0; width: ${event.currentTarget.clientWidth - 7.2 * 2}px;
+        height: ${event.currentTarget.clientHeight - 6}px; font-weight: bold;" />
+    `;
+    
+    /*
+     * this element disables clicking on any other
+     * element on the page, when it is clicked
+     * the user cannot edit the property of the task anymore
+    */ 
+    $(".stop-editing").css("display", "block")
+
+    // focus on the input inside the property to edit
+    event.currentTarget.firstElementChild.focus()
+    
+    // save the task being edited
+    currentTask.taskElement = event.currentTarget.parentElement;
+    currentTask.propertyElement = event.currentTarget;
+  }
+})
+
+// task description was clicked
+$("#list-toDo").on("click", "li > p", function(event) {
+  // if the description paragraph does not already have an input element inside
+  if (event.currentTarget.firstElementChild === null) {
+    // get an array of tasks
+    let parentChildren = Array.from(event.currentTarget.childNodes);
+
+    // get index of the current task inside the childNodes of #list-toDo
+    currentTask.index = parentChildren.indexOf(event.currentTarget);
+
+    // create an input element inside the description paragraph
+    event.currentTarget.innerHTML = `
+      <input type="text" value="${event.currentTarget.innerText}" 
+        style="border: none; background: none; font-size: inherit; 
+        color: inherit; padding: 0; width: ${event.currentTarget.clientWidth}px;
+        height: ${event.currentTarget.clientHeight}px;" />
+    `;
+
+    /*
+     * this element disables clicking on any other
+     * element on the page, when it is clicked
+     * the user cannot edit the property of the task anymore
+    */ 
+    $(".stop-editing").css("display", "block")
+
+    // focus on the input inside the property to edit
+    event.currentTarget.firstElementChild.focus()
+    
+    // save the task being edited
+    currentTask.taskElement = event.currentTarget.parentElement;
+    currentTask.propertyElement = event.currentTarget;
+
+  }
+})
+
+// stop editing element was clicked
+$(".stop-editing").on("click", function() {
+  // hide stop editing element
+  $(".stop-editing").css("display", "none")
+  
+  // set the value of the element to the value of the inner input
+  currentTask.propertyElement.innerHTML = currentTask.propertyElement.firstElementChild.value;
+  
+  // update the tasks toDo list
+  tasks.toDo = tasks.toDo.map(function(task, toDoTaskIndex) {
+    let { taskElement, index } = currentTask;
+    let parentChildren = Array.from(taskElement.parentNode.childNodes);
+    let currentTaskIndex = parentChildren.indexOf(taskElement);
+
+    if (currentTaskIndex -1 === toDoTaskIndex) {
+      return {
+        date: taskElement.querySelector('span').innerText,
+        text: taskElement.querySelector('p').innerText
+      }
+    }
+
+    return task
+  });
+
+  // save the edited task in localstorage
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+})
 
 // remove all tasks
 $("#remove-tasks").on("click", function() {
